e18a84795fe2a232b811832c1eed64eec0dac2a4
AzureSphere <dustin.jason.lee@gmail.com>
2023-11-21 21:24:59 -0500

    add openssl

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import * as https from "https";
+import * as fs from "fs";
 import * as dotenv from "dotenv";
 import * as express from "express";
 import * as bodyParser from "body-parser";
@@ -10,6 +12,10 @@ import api from "./routes";
 
 // Config
 dotenv.config();
+const credentials = {
+  key: fs.readFileSync("config/server.key"),
+  cert: fs.readFileSync("config/server.cert")
+};
 
 // Variables
 const app = express();
@@ -18,7 +24,7 @@ const port = process.env.PORT;
 // Middleware
 app.use(logger("dev"));
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 passport_verify(passport);
@@ -27,4 +33,6 @@ passport_verify(passport);
 api.get("/", (req: express.Request, res: express.Response) => res.send("The response from the server"));
 app.use("/api", api);
 
-app.listen(port, () => console.log(`>> Server is running on port ${port}`));
+// Listen on port
+https.createServer(credentials, app).listen(443, () => console.log(">> HTTPS Server is running"));
+app.listen(port, () => console.log(`>> HTTP Server is running on port ${port}`));
